046c63f66d8021033d4c708ddbe1b3bb595ce365
최성훈 <chltjdgns1009@gmail.com>
2024-01-06 17:28:05 +0900

    publishing

--- src/pages/layouts/Signup/index.js
+++ src/pages/layouts/Signup/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import * as S from './style'
 
 const SignUp = () => {
+
   return (
-    <div>
-      
-    </div>
+    <S.Layout>
+      <S.Title>회원가입</S.Title>
+      <S.FormLayout>
+        <S.Input placeholder='아이디' />
+        <S.Input placeholder='비밀번호' />
+        <S.Input placeholder='이메일주소' />
+        <S.Input placeholder='닉네임' />
+      </S.FormLayout>
+      <S.Submit>확인</S.Submit>
+    </S.Layout>
   );
 };
 
---
+++ src/pages/layouts/Signup/style.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+
+export const Layout = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+export const Title = styled.h1`
+  position: absolute;
+  top: 14%;
+  color: #000;
+  text-align: center;
+  font-family: Inknut Antiqua;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`
+export const FormLayout = styled.div`
+  position: absolute;
+  top: 30%;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`
+export const Input = styled.input`
+  width: 366px;
+  height: 49px;
+  border-radius: 10px;
+  border: 1px solid #000;
+  background: rgba(217, 217, 217, 0.00);
+  font-size: 17px;
+  padding-left: 15px;
+    &::placeholder {
+      font-family: Inknut Antiqua;
+      font-size: 15px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+`
+export const Submit = styled.button`
+  border: none;
+  position: absolute;
+  bottom: 20%;
+  width: 220px;
+  height: 90px;
+  border-radius: 20px;
+  background: #D9D9D9;
+  text-align: center;
+  font-family: Inknut Antiqua;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+    &:active {
+      background-color: #BDBDBD;
+    }
+`
