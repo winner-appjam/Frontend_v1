392d244bcc5737eb31c135f1d8712346f1349910
최성훈 <chltjdgns1009@gmail.com>
2024-01-06 16:46:03 +0900

    signin & signup

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from './layouts/SignIn';
+import SignUp from './layouts/Signup';
+
+const Home = () => {
+  return (
+    <>
+      <SignIn />
+      <SignUp />
+    </>
+  );
+};
+
+export default Home;
